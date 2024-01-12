55afc489206fe3bda0181717b422538002d55799
Júlio Freitas <jcsr.frts@gmail.com>
2024-01-12 13:27:24 -0300

    Novo projeto webapi com controllers + dados mockados na classe

---
+++ server/ChatAPI/Users/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatAPI.Users;
+
+public class User(Guid id, string name, string profilePicture)
+{
+    public Guid Guid { get; private set; } = id;
+    public string Name { get; private set; } = name;
+    public string ProfilePicture { get; private set; } = profilePicture;
+}
---
+++ server/ChatAPI/Users/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAPI.Users;
+
+[ApiController, Route("api/users")]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var listUsers = UserFakeDb.Users;
+        return Ok(listUsers);
+    }
+}
+
