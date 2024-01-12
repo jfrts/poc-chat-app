cb37bc17ca53fff88eb874e8e4fd3edafa698126
Júlio Freitas <jcsr.frts@gmail.com>
2024-01-12 13:34:41 -0300

    Movido a pasta users para dentro de src

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginPageComponent } from '../users/page/login-page/login-page.component';
+import { LoginPageComponent } from './users/page/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
